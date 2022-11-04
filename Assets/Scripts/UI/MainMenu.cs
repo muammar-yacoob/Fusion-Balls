7dc1995fd79aea32b90c37c3b4e846c5d3719670
Muammar81 <Muammar.Yacoob@gmail.com>
2022-11-04 14:02:42 +0000

    Main menu with nick names saved locally

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,10 +11,45 @@ namespace Born.FusionTest
         [SerializeField] private Button teacherButton;
         [SerializeField] private Button studentButton;
 
+        [SerializeField] private TMP_InputField nickNameUI;
+
+        private int teacherSceneIndex = 1;
+        private int studentSceneIndex = 2;
+        private const string nickNamePref = "NICK_NAME";
+
         private void Awake()
         {
-            teacherButton.onClick.AddListener(() => SceneManager.LoadScene(1));
-            studentButton.onClick.AddListener(() => SceneManager.LoadScene(2));
+            teacherButton.onClick.AddListener(LoginAsTeacher);
+            studentButton.onClick.AddListener(LoginAsStudent);
+
+            nickNameUI.text = LoadNickName();
         }
+
+        private void LoginAsTeacher()
+        {
+            SaveNickName();
+            SceneManager.LoadScene(teacherSceneIndex);
+        }
+
+       private void LoginAsStudent()
+        {
+            SaveNickName();
+            SceneManager.LoadScene(studentSceneIndex);
+        }
+
+       private string LoadNickName()
+       {
+           var nickName = PlayerPrefs.GetString(nickNamePref);
+           nickName = String.IsNullOrEmpty(nickName) ? System.Environment.UserName : nickName;
+           return nickName;
+       }
+       
+       private void SaveNickName()
+       {
+           var nickName = nickNameUI.text;
+           nickName = String.IsNullOrEmpty(nickName) ? System.Environment.UserName : nickName;
+           nickName = nickName.ToUpper();
+           PlayerPrefs.SetString(nickNamePref, nickName);
+       }
     }
 }
---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Born.FusionTest.Utils
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+        protected virtual void Awake()
+        {
+            if(Instance != null) Destroy(gameObject);
+            Instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        protected void OnApplicationQuit()
+        {
+            Instance = null;
+            Destroy(gameObject);
+        }
+    }
+}
